badaa78f8044d664e83e508af20801b5bdf89adf
murat.benli <murat.benli@bilgeadam.com>
2020-08-21 10:54:27 +0300

    Updates

---
+++ precache-manifest.911e33d1e1444aeb0e2e07cbf3cd407d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c121901b15ed1b5135fc48e022a748",
+    "url": "/primetodo/index.html"
+  },
+  {
+    "revision": "33e55de1f05fbd4394a2",
+    "url": "/primetodo/static/css/2.958e21da.chunk.css"
+  },
+  {
+    "revision": "7802781d6a6402d35cfd",
+    "url": "/primetodo/static/css/main.6b0c89d3.chunk.css"
+  },
+  {
+    "revision": "33e55de1f05fbd4394a2",
+    "url": "/primetodo/static/js/2.b8be14c4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/primetodo/static/js/2.b8be14c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7802781d6a6402d35cfd",
+    "url": "/primetodo/static/js/main.b229b719.chunk.js"
+  },
+  {
+    "revision": "57fbe50388408b70dfe4",
+    "url": "/primetodo/static/js/runtime-main.3f23f76c.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/primetodo/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "38fa65e37dc6b27f11ce1eda1ded59a7",
+    "url": "/primetodo/static/media/done.38fa65e3.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/primetodo/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/primetodo/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/primetodo/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/primetodo/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/primetodo/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/primetodo/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/primetodo/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/primetodo/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/primetodo/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/primetodo/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/primetodo/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/primetodo/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/primetodo/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/primetodo/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/primetodo/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/primetodo/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/primetodo/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/primetodo/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/primetodo/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/primetodo/static/media/primeicons.79432094.woff"
+  },
+  {
+    "revision": "20908dff039568168422741daa267f83",
+    "url": "/primetodo/static/media/todo.20908dff.png"
+  }
+]);
